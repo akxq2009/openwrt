a55127679c48c12367b3b68a6cfbf3406690292f
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-11-17 16:11:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,15 +13,16 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo "src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld.git" >> "feeds.conf.default"
-echo 'src-git passwall https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall.git' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall2.git' >>feeds.conf.default
+#echo "src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld.git" >> "feeds.conf.default"
+#echo 'src-git passwall https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall.git' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall2.git' >>feeds.conf.default
+echo "src-git https://github.com/haiibo/openwrt-packages.git" >> "feeds.conf.default"
 
 
 #echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 #argon主题
-rm -rf package/lean/luci-theme-argon/
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon/
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # 设置首次登录后台密码为空（进入openwrt后自行修改密码）
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
