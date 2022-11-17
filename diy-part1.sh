3e841dbe0dd39502cf469dccad6bf416d3957e34
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-11-17 15:55:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,12 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+##echo "src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld.git" >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-git clone https://github.com/haiibo/openwrt-packages package/openwrt-packages
+echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 
 #argon主题
 rm -rf package/lean/luci-theme-argon/
