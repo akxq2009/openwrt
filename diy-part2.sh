419f3ae1c90bbbe6b62e9c78e23ee0da483a5455
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-11-17 19:28:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,10 +19,10 @@ sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.co
 
 
 # 设置首次登录后台密码为空（进入openwrt后自行修改密码）
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
+#sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
 
 # 删除默认防火墙
-sed -i '/to-ports 53/d' "${ZZZ_PATH}"
+#sed -i '/to-ports 53/d' "${ZZZ_PATH}"
 
 # 取消路由器每天跑分任务
-sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
+#sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
