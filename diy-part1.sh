2db14e8e11ad63f26fe3a0f2dc46db53dbaa99c4
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-11-17 17:06:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,10 +10,10 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo "src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld.git" >> "feeds.conf.default"
+#echo "src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld.git" >> "feeds.conf.default"
 echo 'src-git passwall https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall.git' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall2.git' >>feeds.conf.default
 
@@ -22,12 +22,10 @@ echo 'src-git passwall2 https://github.com/haiibo/openwrt-packages/tree/master/l
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # 设置首次登录后台密码为空（进入openwrt后自行修改密码）
-#sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
-
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
 
 # 删除默认防火墙
-#sed -i '/to-ports 53/d' "${ZZZ_PATH}"
-
+sed -i '/to-ports 53/d' "${ZZZ_PATH}"
 
 # 取消路由器每天跑分任务
-#sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
+sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
