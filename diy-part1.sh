158f7471b124ac5f9538c1530090b1e3fbf83251
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-11-17 17:45:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo "src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld" >> "feeds.conf.default"
