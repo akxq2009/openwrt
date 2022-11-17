5d62d881d02b029c0bf7722a9130231115e51ab0
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-11-17 22:29:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld' >> "feeds.conf.default"
-echo 'src-git passwall https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall2' >>feeds.conf.default
-echo 'src-git argon https://github.com/haiibo/openwrt-packages/tree/master/luci-theme-argon' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/haiibo/openwrt-packages/tree/master/helloworld' >> "feeds.conf.default"
+#echo 'src-git passwall https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/haiibo/openwrt-packages/tree/master/luci-app-passwall2' >>feeds.conf.default
+#echo 'src-git argon https://github.com/haiibo/openwrt-packages/tree/master/luci-theme-argon' >>feeds.conf.default
 
