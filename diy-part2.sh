71b49aa322863f951e94eef1305e6cfadd2d4455
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-12-01 16:55:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,10 @@ sed -i 's/192.168.1.1/192.168.123.88/g' package/base-files/files/bin/config_gene
 
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
-#git clone https://github.com/haiibo/openwrt-packages package/openwrt-packages
-git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
+
+#git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
+
+git clone https://github.com/kenzok8/small-package package/small-package
 
 # 设置首次登录后台密码为空（进入openwrt后自行修改密码）
 #sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
