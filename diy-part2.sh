669d977da0060838918953783245848a1c2fec55
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2022-12-21 23:55:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,20 +11,12 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.88/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.6/g' package/base-files/files/bin/config_generate
 
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
+#git clone https://github.com/kenzok8/small-package package/small-package
 
 #git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
 
-git clone https://github.com/kenzok8/small-package package/small-package
 
-# 设置首次登录后台密码为空（进入openwrt后自行修改密码）
-#sed -i '/CYXluq4wUazHjmCDBCqXF/d' "${ZZZ_PATH}"
-
-# 删除默认防火墙
-#sed -i '/to-ports 53/d' "${ZZZ_PATH}"
-
-# 取消路由器每天跑分任务
-#sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "${FIN_PATH}"
