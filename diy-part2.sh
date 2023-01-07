e7cee3dcd9e29914d66c97c14ceadbc912415087
akxq2009 <90573592+akxq2009@users.noreply.github.com>
2023-01-07 17:18:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,8 @@ sed -i 's/192.168.1.1/192.168.123.6/g' package/base-files/files/bin/config_gener
 
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
-#git clone https://github.com/kenzok8/small-package package/small-package
+git clone https://github.com/kenzok8/small-package package/small-package
 
-#git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
+git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
 
-
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
